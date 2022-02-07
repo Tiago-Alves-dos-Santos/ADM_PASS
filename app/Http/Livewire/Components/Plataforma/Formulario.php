f3eb66ce79c6eccf718d8b1289622e5b17dd7777
Tiago Alves <tiagooliveiraaso@gmail.com>
2022-02-07 10:02:42 -0300

    iniciado codigo create plataforma

--- app/Http/Livewire/Components/Plataforma/Formulario.php
+++ app/Http/Livewire/Components/Plataforma/Formulario.php
@@ -3,9 +3,33 @@
 namespace App\Http\Livewire\Components\Plataforma;
 
 use Livewire\Component;
+use App\Models\Plataforma;
 
 class Formulario extends Component
 {
+    public $id_plataforma = 0;
+    public $plataforma = null;
+    public $opcao = ["cadastrar","editar","deletar"];
+    public function mount()
+    {
+        # code...
+    }
+
+    public function createPlataforma()
+    {
+        try {
+            if(!Plataforma::verficarExistencia($this->plataforma, $opcao[0])){
+                Plataforma::create([
+                    "plataforma" => $this->plataforma
+                ]);
+            }else{//caso exista
+                
+            }
+        } catch (\Exception $e) {
+            
+        }
+    }
+
     public function render()
     {
         return view('livewire.components.plataforma.formulario');
--- app/Models/Plataforma.php
+++ app/Models/Plataforma.php
@@ -10,4 +10,16 @@ class Plataforma extends Model
 {
     use HasFactory;
     use SoftDeletes;
+    public static function verficarExistencia($plataforma,$opcao, $id_plataforma = 0)
+    {
+        if($opcao === "cadastrar"){
+            return Plataforma::where('plataforma',$plataforma)->exists();
+        }else if($opcao === "editar" && $id_plataforma <= 0){
+            throw new \Exception("Parametro id_plataforma inválido na opção editar", 1);
+        }else if($opcao === "editar"){
+            return Plataforma::where('id','!=',$id_plataforma)->where('plataforma',$plataforma)->exists();
+        }else{
+            throw new \Exception("Parametro opção com valor não reconhecido na condição, valor: ".$opcao, 1);
+        }
+    }
 }
