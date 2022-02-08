3aad2c78a6516159a2e569fe7699a7e1a99b6e1d
Tiago Alves <tiagooliveiraaso@gmail.com>
2022-02-08 16:56:55 -0300

    component form-filtro criado

---
+++ app/Http/Livewire/Components/Conta/FormFiltro.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Components\Conta;
+
+use Livewire\Component;
+
+class FormFiltro extends Component
+{
+    public function render()
+    {
+        return view('livewire.components.conta.form-filtro');
+    }
+}
